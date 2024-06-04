1c7e229e92b04207d644c69411ed0577c720f142
João Pedro de Melo Cabana <jpcabana1@gmail.com>
2024-06-04 14:40:37 -0300

    feat: asyncio

--- basics/run.py
+++ basics/run.py
@@ -1,4 +1,23 @@
+'''
+# Patterns Example
 from classes.Patterns.Factory.AnimalFactory import AnimalFactory
 
 if __name__ == "__main__":
     print(AnimalFactory().create("Cat").speak())
+'''
+
+import asyncio
+
+async def await_for(time: int) -> str:
+    await asyncio.sleep(time)
+    return f"Finished {time}s."
+
+async def main():
+   gather_return = await asyncio.gather(
+        await_for(4),
+        await_for(5)
+    )
+   print(gather_return[1])
+
+if __name__ == "__main__":
+    asyncio.run(main())
