77738ad7cf459d744a32f2ed2af8603ed15231a6
João Pedro de Melo Cabana <jpcabana1@gmail.com>
2024-07-04 11:23:57 -0300

    feat: transcribe-refactor

--- vector-store/transcribe.py
+++ vector-store/transcribe.py
@@ -2,9 +2,7 @@ import os
 import re
 import requests
 import boto3
-from dotenv import load_dotenv
 from asyncio import sleep
-from asyncio import run
 import datetime
 
 class TranscribeService:
@@ -59,14 +57,3 @@ class TranscribeService:
         response = self.__client.delete_transcription_job(TranscriptionJobName=job_name)
         
         return transcription_job_response.json()["results"]["transcripts"][0]["transcript"]
-    
-async def main():
-    video_uri = "s3://test-bucket-plataforma/3f80ee70-2367-11eb-a3ad-9f132a537bed_S20-2-PLA61-9-01-OAU-003.mp4"
-    language_code="pt-BR"
-    media_format="mp4"
-    transcription = await TranscribeService().transcribe_video(video_uri=video_uri, language_code=language_code, media_format=media_format)
-    print(transcription)
-
-if __name__ == "__main__":
-    load_dotenv()
-    run(main())
